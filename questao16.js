347d8c3e6685237140e5d809a3b536f4dee695fc
Danie1zika <danielbrandao03420@gmail.com>
2024-03-27 08:45:01 -0400

    questao 16 finalizada

---
+++ questao16.js
@@ -0,0 +1,6 @@
+// Questão 16: Como você pode criar um objeto vazio em JavaScript?
+
+let minhaString = "Olá Mundo!";
+let comprimento = minhaString.length;
+
+console.log(comprimento); // Saída: 10
