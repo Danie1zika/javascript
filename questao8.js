aa3545531fa624a47fa74141fed80ccae700470f
Danie1zika <danielbrandao03420@gmail.com>
2024-03-27 08:44:03 -0400

    questao 8 finalizada

---
+++ questao8.js
@@ -0,0 +1,6 @@
+// Questão 8: Como você pode acessar o último elemento de um array em JavaScript?
+
+const myArray = ["Maça", "Banana", ]
+var quantidade = myArray.length-1;
+console.log(myArray);
+console.log(quantidade);
